b879cf41babe8ac10ab80b85011eff5f7fb6fbd6
R-dVL <rauldel.valle.lima@gmail.com>
2024-03-05 17:12:15 +0100

    OAuth 1st version

--- api/v1/endpoints/auth.py
+++ api/v1/endpoints/auth.py
@@ -18,27 +18,13 @@ from v1.models.user import UserModel, UpdateUserModel, UserCollection
 
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.bandanize
-bands_collection = db.get_collection("users")
+users_collection = db.get_collection("users")
 
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-        "photo": "",
-        "band_ids": []
-    }
-}
-
 
 class Token(BaseModel):
     access_token: str
@@ -66,15 +52,16 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
+async def get_user(db, username: str):
+    if (
+        song := await db.find_one({"username": username})
+    ) is not None:
+        user_dict = song
         return UserInDB(**user_dict)
 
 
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
+async def authenticate_user(db, username: str, password: str):
+    user = await get_user(db, username)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
@@ -107,7 +94,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = await get_user(users_collection, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -129,7 +116,7 @@ async def get_current_active_user(
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = await authenticate_user(users_collection, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
