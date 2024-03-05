9e3763bfb6e673460f1580d2a8289c802d72685b
R-dVL <rauldel.valle.lima@gmail.com>
2024-03-05 15:42:45 +0100

    Complete User model

--- api/v1/models/user.py
+++ api/v1/models/user.py
@@ -37,26 +37,24 @@ class UserModel(BaseModel):
     )
 
 class UpdateUserModel(BaseModel):
-    name: Optional[str] = None
+    username: Optional[str] = None
+    full_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    hashed_password: Optional[List[str]] = None
     photo: Optional[str] = None
-    description: Optional[str] = None
-    song_ids: Optional[List[str]] = None
-    user_ids: Optional[List[str]] = None
+    band_ids: Optional[List[str]] = None
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
         json_encoders = {ObjectId: str},
         json_schema_extra = {
             "example": {
-                "name": "The Sodawaves",
-                "photo": "...",
-                "description": "Indie Rock user from Algeciras",
-                "song_ids": [
-                    "65e6d5cb2258227facc570c1",
-                    "..."
-                ],
-                "user_ids": [
-                    "65e6d5cb2258227facc570c1",
-                    "65e6d5cb2258227facc570c0",
+                "username": "lizard_king",
+                "full_name": "Jim Morrison",
+                "email": "li.king@thedoors.com",
+                "hashed_password": "latuyaporsiacaso",
+                "disabled": False,
+                "photo": "empty",
+                "band_ids": [
                     "..."
                 ]
             }
